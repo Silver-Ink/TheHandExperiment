67568726404bb01bd662fa73a50f49977d68f0e6
Audrey-Sanchez <124395245+Sherlock-AS@users.noreply.github.com>
2025-03-26 10:37:04 -0400

    Mini fix

---
+++ TheHandExperiment/Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        var newForward = (-1 * transform.position + _camera.transform.position).normalized;
+        //transform.LookAt(_camera.transform);
+        transform.forward = newForward;
+        Debug.Log(newForward);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
